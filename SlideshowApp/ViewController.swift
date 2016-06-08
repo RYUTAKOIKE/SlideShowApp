39d6432d02ed601963206e7b50ebbb7c498a47a4
小池隆太 <whoopees_25@yahoo.co.jp>
2016-06-08 17:49:24 +0900

    UIVIEWとSETIMAGE(BUTTON)入れた

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,16 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var img: UIImageView!
+    
+    @IBOutlet weak var image: UIButton!
+  
+  
+
+    @IBOutlet weak var next: UIButton!
+  
+ 
+   
+    
+    
+    
+    
+    @IBOutlet weak var back: UIButton!
+    
+    @IBOutlet weak var auto: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+
+      
+      
+        // 表示する画像を設定する.
+        let myImage = UIImage(named: "IMAGE1-A")
+        
+        // 画像をUIImageViewに設定する.
+        img.image = myImage
+        
+        
+        // をViewに追加する.
+        self.view.addSubview(img)
+    
+        
+        
+        image.setImage(UIImage(named:"myimage"), forState: UIControlState.Normal)
+    
+    
+    
+    
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
+        
+        
+       
     }
 
-
+    
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+       
+    }
+    
+    
 }
+
 
